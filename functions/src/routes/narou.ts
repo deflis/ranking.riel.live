e1c39331f57fa74b6f4a732b478270023a110dd0
deflis <deflis@gmail.com>
2020-03-20 11:39:25 +0000

    サーバーサイドでカスタムランキングに対応させる

--- functions/src/routes/narou.ts
+++ functions/src/routes/narou.ts
@@ -1,5 +1,12 @@
 import { Router } from "express";
-import { ranking, rankingHistory, RankingType, search } from "narou";
+import {
+  ranking,
+  rankingHistory,
+  RankingType,
+  search,
+  Order,
+  RankingResult
+} from "narou";
 import { parseISO, formatISO } from "date-fns";
 
 const router = Router();
@@ -50,6 +57,50 @@ router.get("/detail/:ncode", async (req, res) => {
 
     res.set("Cache-Control", "public, max-age=300, s-maxage=600");
     res.json({ detail, ranking });
+  } catch (e) {
+    console.error(e);
+    res.status(500).json(e);
+  }
+});
+
+router.get("/custom/:order", async (req, res) => {
+  try {
+    const order = req.params.order as Order;
+    const { keyword, biggenre, genre } = req.query;
+
+    const searchBuilder = search();
+    searchBuilder.order(order).limit(500);
+
+    if (keyword) {
+      searchBuilder.word(keyword).byKeyword(true);
+    }
+    if (biggenre) {
+      searchBuilder.bigGenre(biggenre);
+    }
+    if (genre) {
+      searchBuilder.genre(genre);
+    }
+
+    const searchResult = await searchBuilder.execute();
+
+    const ranking: RankingResult[] = searchResult.values.map((value, index) => {
+      let pt = value.global_point;
+      if (order === Order.DailyPoint) {
+        pt = value.daily_point;
+      } else if (order === Order.WeeklyPoint) {
+        pt = value.weekly_point;
+      } else if (order === Order.MonthlyPoint) {
+        pt = value.monthly_point;
+      } else if (order === Order.QuarterPoint) {
+        pt = value.quarter_point;
+      } else if (order === Order.YearlyPoint) {
+        pt = value.yearly_point;
+      }
+      return { ...value, rank: index + 1, pt };
+    });
+
+    res.set("Cache-Control", "public, max-age=300, s-maxage=600");
+    res.json(ranking);
   } catch (e) {
     console.error(e);
     res.status(500).json(e);
