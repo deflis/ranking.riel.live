cd518f1ea38e10b311392c435840d1538a1a9c2d
deflis <deflis@gmail.com>
2021-05-11 04:46:18 +0900

    エラー表示修正

--- src/api/useR18Detail.ts
+++ src/api/useR18Detail.ts
@@ -11,15 +11,12 @@ type Result = {
 
 export function useR18Detail(ncode: string) {
   const { data, error } = useSWR<Result>(`/_api/r18detail/${ncode}`, fetcher);
-  const loading = !data;
-  if (error) {
-    throw error;
-  }
+  const loading = !data && !error;
 
   return {
     result: data?.detail ? data : undefined,
     loading,
-    error: data?.detail ? undefined : data,
+    error,
   };
 }
 
