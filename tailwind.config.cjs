23deaa6a6c3cd973b3d6ab64727555040d771ea6
deflis <deflis@gmail.com>
2022-07-25 22:26:24 +0900

    テーマ編集の準備

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,6 +7,7 @@ module.exports = {
       fontFamily: {
         sans: ['"Noto Sans JP"', "sans-serif"],
       },
+      colors: (theme) => ({}),
     },
   },
   plugins: [require("@tailwindcss/forms")],
