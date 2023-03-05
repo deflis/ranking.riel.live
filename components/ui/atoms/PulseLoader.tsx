deb865632ef796aa62e50c9cf12aff2cc5e8c9c0
deflis <deflis@gmail.com>
2023-03-06 05:31:00 +0900

    pの中に入れることを想定する

--- components/ui/atoms/PulseLoader.tsx
+++ components/ui/atoms/PulseLoader.tsx
@@ -2,9 +2,9 @@ import clsx from "clsx";
 import { ComponentProps } from "react";
 
 export const PulseLoader: React.FC<
-  ComponentProps<"div"> & { disabled?: boolean }
+  ComponentProps<"span"> & { disabled?: boolean }
 > = ({ className, disabled = false, ...props }) => (
-  <div
+  <span
     className={clsx(
       !disabled && "animate-pulse",
       "w-full bg-slate-700 rounded h-4 inline-block",
