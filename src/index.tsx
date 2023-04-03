5da101009d3023eeff69233b0fa6c91e2676d140
deflis <deflis@gmail.com>
2023-04-04 04:15:14 +0900

    service workerにはいなくなってもらおう

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register({
-  onUpdate: (registration) => {
-    const waitingServiceWorker = registration.waiting;
-
-    if (waitingServiceWorker) {
-      waitingServiceWorker.addEventListener("statechange", (event) => {
-        if ((event.target as any)?.state === "activated") {
-          window.location.reload();
-        }
-      });
-      waitingServiceWorker.postMessage({ type: "SKIP_WAITING" });
+navigator.serviceWorker.getRegistrations()
+  .then(registrations => {
+    for (let registration of registrations) {
+      registration.unregister();
     }
-  },
-});
+  })
