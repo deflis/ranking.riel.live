04dab74e3e3a762eec7e9bedb539f8d83b5bd6ad
deflis <deflis@gmail.com>
2023-11-08 01:38:13 +0900

    remove service worker

--- public/service-worker.js
+++
@@ -1,6 +0,0 @@
-// Service Workerを削除する
-navigator.serviceWorker.getRegistrations().then(function (registrations) {
-  for (let registration of registrations) {
-    registration.unregister();
-  }
-});
