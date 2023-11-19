8a268493158f3178308fd28eebd1e0ffe7d72533
deflis <deflis@gmail.com>
2023-11-20 05:21:21 +0900

    画像もlazy loadingする

--- src/components/ui/common/SelfAd.tsx
+++ src/components/ui/common/SelfAd.tsx
@@ -6,7 +6,15 @@ export const SelfAd: React.FC = () => {
   return (
     <div className="flex justify-center">
       <a href="https://riel.live" target="_blank" rel="noreferrer">
-        <img className="max-w-full" src={Ad} alt="私が作りました" />
+        <img
+          className="max-w-full"
+          src={Ad}
+          alt="私が作りました"
+          width={970}
+          height={250}
+          decoding="async"
+          loading="lazy"
+        />
       </a>
     </div>
   );
