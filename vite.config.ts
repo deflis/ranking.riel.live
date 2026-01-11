aeb56ad0f12d707a49e6c324dbeb9f869886921a
deflis <deflis@gmail.com>
2026-01-11 18:07:16 +0900

    vite.config.tsにCloudflareプラグインを追加
    - SSR環境の設定を追加

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,12 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from "vite";
 import react from '@vitejs/plugin-react'
+import { cloudflare } from "@cloudflare/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    cloudflare({ viteEnvironment: { name: 'ssr' } }),
     tailwindcss(),
     tanstackStart({
       srcDirectory: 'src',
