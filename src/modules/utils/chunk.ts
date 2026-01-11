dbdae6070f1ceb89e6295eed4e05af4e505da8b1
deflis <deflis@gmail.com>
2026-01-11 17:54:12 +0900

    chunk関数にパフォーマンス向上のためのコメントを追加

--- src/modules/utils/chunk.ts
+++ src/modules/utils/chunk.ts
@@ -1,5 +1,6 @@
 export function chunk<T>(arr: readonly T[], size: number): T[][] {
   return arr.reduce(
+    // biome-ignore lint/performance/noAccumulatingSpread: <explanation>
     (newarr, _, i) => (i % size ? newarr : [...newarr, arr.slice(i, i + size)]),
     [] as T[][]
   );
