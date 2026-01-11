2ff03a726176d8c39236454b4e78b7d4828a1df6
deflis <deflis@gmail.com>
2026-01-11 23:55:08 +0900

    不要なAPI関数とサービスワーカーを削除
    - functions/_api/[[catchall]].jsを削除
    - functions/service-worker.js.jsを削除

--- functions/_api/[[catchall]].js
+++
@@ -1,6 +0,0 @@
-export function onRequest(context) {
-  const origin = new URL( context.request.url);
-  origin.hostname = "narou-ranking.web.app";
-  return fetch(origin.toString(), context.request);
-
-}
