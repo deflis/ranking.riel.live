f19753d65488602df407116c75f4dfd29ec2822c
deflis <deflis@gmail.com>
2026-03-05 23:59:43 +0900

    vite.config.tsにexcludePathsを追加し、prerender設定を更新

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,15 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+const excludePaths = [
+	"/custom/",
+	"/detail/",
+	"/ranking/d/",
+	"/ranking/w/",
+	"/ranking/m/",
+	"/ranking/q/",
+]
+
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
@@ -14,6 +23,8 @@ export default defineConfig({
 			srcDirectory: "src",
 			prerender: {
 				enabled: true,
+        crawlLinks: true,
+				filter: ({ path }) => !excludePaths.some(excludePath => path.startsWith(excludePath)),
 			},
 		}),
 		react(),
