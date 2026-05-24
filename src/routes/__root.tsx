076c5dc8991736704f7a7d2b0a9a8f661cef9468
deflis <deflis@gmail.com>
2026-05-24 16:15:28 +0900

    RootDocumentコンポーネントにviewportメタタグを追加

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -57,6 +57,7 @@ function RootDocument({ children }: { children: React.ReactNode }) {
 		<html lang="ja">
 			<head>
 				<meta charSet="utf-8" />
+				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 				<HeadContent />
 				<script
 					async
